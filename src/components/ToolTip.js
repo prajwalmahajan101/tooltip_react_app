b26cc254eb6e925fc7e4500f807c03711b4ff812
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2022-07-06 16:50:29 +0530

    Final

--- src/components/ToolTip.js
+++ src/components/ToolTip.js
@@ -1,10 +1,13 @@
 
-import Tooltip from '@mui/material/Tooltip';
+//Bootsrap Files
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
+//ToolTip From Material UI
+import Tooltip from '@mui/material/Tooltip';
+// Create Theme
 import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 const defaultTheme = createTheme();
 const theme = createTheme({
@@ -29,10 +32,9 @@ let ToolTip = (props) =>{
                     title={props.position}
                     placement={props.position}
                     arrow={true}
-                    // followCursor={true}
                     leaveDelay={200}
                 >
-                    <button className={"btn btn-dark text-light but"}>{props.children}</button>
+                    <button className={"btn btn-dark text-light buttons"}>{props.children}</button>
                 </Tooltip>
             </ThemeProvider>
         </div>
